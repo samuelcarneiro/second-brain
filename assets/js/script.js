45409496cf6d487633b340e2684aa375154fc6c1
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-09-04 10:53:29 -0300

    Adicionado a estilização da página em variáveis e adicionado alguns assuntos para navegação entre links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 /* Mostrar o ano atual no footer */
-//Pega o elemento span#mostrarAnoAtual
-const ano = document.getElementById("mostrarAnoAtual");
-// Pega a data atual
-const anoAtual = new Date();
-//Colocar o ano atual no span
-ano.innerHTML = anoAtual.getFullYear();
+const ano = document.getElementById("mostrarAnoAtual");//Pega o elemento span#mostrarAnoAtual
+const anoAtual = new Date();// Pega a data atual
+ano.innerHTML = anoAtual.getFullYear();//Colocar o ano atual no span
